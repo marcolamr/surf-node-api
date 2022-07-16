bd0b6beca012b2c30421f3363f5719bb7e939acd
Marcos Roberto <marcola.mr@gmail.com>
2022-07-16 14:28:59 -0300

    chore: Users Module - create Controller and Model

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import './util/module-alias';
+import '@src/util/module-alias';
 
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
-import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
-import { BeachesController } from './controllers/beaches';
+import { ForecastController } from '@src/controllers/forecast';
+import { BeachesController } from '@src/controllers/beaches';
+import { UsersController } from '@src/controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -39,7 +40,12 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
+    const usersController = new UsersController();
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async setupDatabase(): Promise<void> {
