56c2f87bb9ddea16fcf6e3da76807f9dce78f501
Marcos Roberto <marcola.mr@gmail.com>
2022-07-15 21:22:07 -0300

    chore: jest mock with typescript

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,13 @@ import stormGlassWeatherNormalized3HoursFixture from '@test/fixtures/stormglass_
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.209624;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
