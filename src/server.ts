bc9c7cda29e739eb3201c8ad1b35e7beb6916ae5
Marcos Roberto <marcola.mr@gmail.com>
2022-07-16 11:49:28 -0300

    chore: config mongodb with nodejs and typescript

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,25 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase();
   }
 
   public getApp(): Application {
     return this.app;
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   private setupExpress(): void {
@@ -26,5 +32,9 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect();
   }
 }
