71991f02a49deb81d10f98241ec9d8b9d12595ef
Marcos Roberto <marcola.mr@gmail.com>
2022-07-17 16:20:38 -0300

    chore: order forecasts by rating

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -11,7 +11,7 @@ jest.mock('@src/clients/stormGlass');
 describe('Forecast Service', () => {
   const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
 
-  it('should return the forecast for mutiple beaches in the same hour with different ratings', async () => {
+  it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating', async () => {
     mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
       {
         swellDirection: 123.41,
@@ -24,7 +24,6 @@ describe('Forecast Service', () => {
         windSpeed: 100,
       },
     ]);
-
     mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
       {
         swellDirection: 64.26,
@@ -37,7 +36,6 @@ describe('Forecast Service', () => {
         windSpeed: 100,
       },
     ]);
-
     const beaches: Beach[] = [
       {
         lat: -33.792726,
@@ -54,26 +52,10 @@ describe('Forecast Service', () => {
         userId: 'fake-id',
       },
     ];
-
     const expectedResponse = [
       {
         time: '2020-04-26T00:00:00+00:00',
         forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100,
-          },
           {
             lat: -33.792726,
             lng: 141.289824,
@@ -89,13 +71,26 @@ describe('Forecast Service', () => {
             windDirection: 299.45,
             windSpeed: 100,
           },
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100,
+          },
         ],
       },
     ];
-
     const forecast = new Forecast(mockedStormGlassService);
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
-
     expect(beachesWithRating).toEqual(expectedResponse);
   });
 
