8307f6c85972450d88e73b15acca23aad5dbd3a8
Marcos Roberto <marcola.mr@gmail.com>
2022-07-17 19:08:38 -0300

    chore: create middleware for handling errors OpenApi

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import * as database from '@src/database';
-import expressPino from 'express-pino-logger';
+import expressPino, { HttpLogger } from 'express-pino-logger';
 import cors from 'cors';
+import apiSchema from './api.schema.json';
+import swaggerUi from 'swagger-ui-express';
+import { OpenApiValidator } from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
 import { ForecastController } from '@src/controllers/forecast';
 import { BeachesController } from '@src/controllers/beaches';
 import { UsersController } from '@src/controllers/users';
 import logger from './logger';
+import { apiErrorValidator } from '@src/middlewares/api-error-validator';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -18,8 +23,10 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.setupDocs();
     this.setupControllers();
     await this.setupDatabase();
+    this.setupErrorHandlers();
   }
 
   public getApp(): Application {
@@ -38,11 +45,11 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
-    // this.app.use(
-    //   expressPino({
-    //     logger,
-    //   })
-    // );
+    this.app.use(
+      expressPino({
+        logger: logger as any,
+      })
+    );
     this.app.use(
       cors({
         origin: '*',
@@ -63,5 +70,19 @@ export class SetupServer extends Server {
 
   private async setupDatabase(): Promise<void> {
     await database.connect();
+  }
+
+  private setupErrorHandlers(): void {
+    this.app.use(apiErrorValidator);
+  }
+
+  private async setupDocs(): Promise<void> {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
+
+    await new OpenApiValidator({
+      apiSpec: apiSchema as OpenAPIV3.Document,
+      validateRequests: false,
+      validateResponses: false,
+    }).install(this.app);
   }
 }
