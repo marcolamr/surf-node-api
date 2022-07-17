bdca0e588f8d6ae5467d5b2c43a81a893e6a07f6
Marcos Roberto <marcola.mr@gmail.com>
2022-07-17 15:36:32 -0300

    chore: end rating algorithm

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -4,7 +4,7 @@ import {
   Forecast,
   ForecastProcessingInternalError,
 } from '@src/services/forecast';
-import { Beach, BeachPosition } from '@src/models/beach';
+import { Beach, GeoPosition } from '@src/models/beach';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -21,7 +21,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
+        position: GeoPosition.E,
         userId: 'fake-user-id',
       },
     ];
@@ -106,7 +106,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
+        position: GeoPosition.E,
         userId: 'fake-user-id',
       },
     ];
